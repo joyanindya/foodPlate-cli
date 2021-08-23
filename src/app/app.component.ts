ab704898deb504a2aa04c7549954c02e3dc87b1a
Anindya <anindya.das@jbhunt.com>
2021-08-23 16:07:45 -0500

    custom service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { User } from './models/User';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'fp-app',
@@ -9,10 +11,13 @@ import { Title } from '@angular/platform-browser';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private titleService: Title) {
+  user: User;
+
+  constructor(private titleService: Title, private userService: UserService) {
 
   }
   ngOnInit() {
     this.titleService.setTitle('Welcome to Food Plate')
+    this.user = this.userService.getUser();
   }
 }
